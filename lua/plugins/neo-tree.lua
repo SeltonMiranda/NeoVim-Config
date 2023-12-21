870a0ddb5539b57b755659c0ece771ff747d3065
Selton <seltonmiranda00@gmail.com>
2023-12-20 21:24:02 -0300

    added lsp

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,12 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-	 visible = true,
-	 show_hidden_count = true,
-	 hide_dotfiles = false,
-	 hide_gitignored = true,
-	 hide_by_name = {
-	   -- '.git',
-	   -- '.DS_Store',
-	   -- 'thumbs.db',
-	 },
-	never_show = {},
-      },
-    }
-  },
+    opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+          },
+        },
+    },
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",

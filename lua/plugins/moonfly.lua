5e653db9b33592c5d6276bff72b9d339d70ded47
Selton <seltonmiranda00@gmail.com>
2024-01-27 17:55:42 -0300

    new colorscheme

--- lua/plugins/moonfly.lua
+++ lua/plugins/moonfly.lua
@@ -12,6 +12,8 @@ return {
   },
       config = function()
        vim.cmd[[colorscheme moonfly]]
+       vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+       vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
       end
   }
 

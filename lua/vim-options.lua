6924f9fd59cf7dd820dcf080146a65800951b1c3
Selton <seltonmiranda00@gmail.com>
2024-01-27 17:57:55 -0300

    increased tab shift for C projects

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=8")
+vim.cmd("set softtabstop=8")
+vim.cmd("set shiftwidth=8")
 vim.cmd("set number");
 vim.g.mapleader = " "
-vim.g.monokaipro_filter = "machine"
 

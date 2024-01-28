1bcccba219a441873b2f4dcb485830d72091231b
Selton <seltonmiranda00@gmail.com>
2024-01-28 14:15:20 -0300

    added keybinding to move line up/down

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,7 @@ vim.cmd("set shiftwidth=8")
 vim.cmd("set number");
 vim.g.mapleader = " "
 
+
+-- some keybindings
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==') --move line down
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==') --move line up

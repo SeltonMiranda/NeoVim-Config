6a0e554f4f8968a7222389742263bfdafc9fdb24
Selton <seltonmiranda00@gmail.com>
2024-02-06 21:29:36 -0300

    changed colorscheme

--- lua/plugins/moonfly.lua
+++ lua/plugins/moonfly.lua
@@ -1,6 +1,7 @@
 return {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
+    --"bluz71/vim-moonfly-colors",
+    "NLKNguyen/papercolor-theme",
+    --name = "moonfly",
     lazy = false,
     priority = 1000,
     opts = {
@@ -11,9 +12,10 @@ return {
       }
   },
       config = function()
-       vim.cmd[[colorscheme moonfly]]
-       vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-       vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+       vim.cmd[[set background=dark]]
+       vim.cmd[[colorscheme PaperColor]]
+       -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+       -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
       end
   }
 

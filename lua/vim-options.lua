7d07df4e3a32581da1283a3d88bd9c9e81fe628a
Selton <seltonmiranda00@gmail.com>
2024-07-24 22:11:24 -0300

    new theme

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,16 @@ vim.cmd("set shiftwidth=8")
 vim.cmd("set number");
 vim.g.mapleader = " "
 
-
 -- some keybindings
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==') --move line down
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==') --move line up
+vim.keymap.set('c', 'jj', '<C-c>')
+vim.keymap.set('c', 'JJ', '<C-c>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'JJ', '<Esc>')
+vim.keymap.set('n', 'S-x', '$')
+vim.keymap.set('n', 'S-c', '0')
+vim.keymap.set('n', 'gge', 'gg=G') -- format code
+
+vim.api.nvim_set_keymap('c', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('c', 'JJ', '<Esc>', { noremap = true, silent = true })

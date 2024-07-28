e3a8d1cd9b1139eccb4e4e3f431d9cda1509fdf7
Selton <seltonmiranda00@gmail.com>
2024-07-28 13:38:20 -0300

    changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=8")
 vim.cmd("set softtabstop=8")
 vim.cmd("set shiftwidth=8")
 vim.cmd("set number");
+vim.cmd("set clipboard+=unnamedplus")
 vim.g.mapleader = " "
 
 -- some keybindings
@@ -18,3 +19,25 @@ vim.keymap.set('n', 'gge', 'gg=G') -- format code
 
 vim.api.nvim_set_keymap('c', 'jj', '<Esc>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('c', 'JJ', '<Esc>', { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "c",
+    callback = function()
+        vim.cmd("set tabstop=8")
+        vim.cmd("set softtabstop=8")
+        vim.cmd("set shiftwidth=8")
+        vim.cmd("set expandtab")
+        vim.cmd("set smarttab")
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "h",
+    callback = function()
+        vim.cmd("set tabstop=8")
+        vim.cmd("set softtabstop=8")
+        vim.cmd("set shiftwidth=8")
+        vim.cmd("set expandtab")
+        vim.cmd("set smarttab")
+    end
+})
